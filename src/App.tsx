70659a0285bfc511f34358a674c63d21a8b17001
AndreaParrini <andrea.parrini26@gmail.com>
2025-04-15 10:48:07 +0200

    add state to manage todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 
 import Todo from "./models/todo";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
 
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript')
-]
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo)
+    })
+  }
   
   return (
     <div>
+        <NewTodo addNewTodo={addTodoHandler}/>
         <Todos items={todos}/>
     </div>
   );
